51dab5c72f40469827d1d7d35da137309c4fcd79
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-12-02 10:37:31 +0100

    Loan Simulator : implement the format display

--- app/src/main/java/com/openclassrooms/realestatemanager/utils/REMHelper.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/utils/REMHelper.java
@@ -1,7 +1,14 @@
 package com.openclassrooms.realestatemanager.utils;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+
+import com.openclassrooms.realestatemanager.R;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
 
 /**
  * Created by Florence LE BOURNOT on 23/11/2020
@@ -9,9 +16,33 @@ import android.widget.ArrayAdapter;
 
 public class REMHelper {
 
+    private static final String TAG = "TAG_REMHelper";
+
     public static ArrayAdapter<CharSequence> paramSpinAdapter(Context pContext, int pResources) {
         ArrayAdapter<CharSequence> lAdapter = ArrayAdapter.createFromResource(pContext,pResources,android.R.layout.simple_spinner_item);
         lAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         return lAdapter;
+    }
+
+    public static String addValueAndSeparatorToString(String pString, String pSeparator, String pValue) {
+        if (pString.length()>0) {
+            pString = pString + pSeparator;
+        }
+        return pString + pValue;
+    }
+
+    public static String formatNumberWithCommaAndCurrency(int pValue) {
+        return NumberFormat.getCurrencyInstance(Locale.getDefault()).format(pValue);
+    }
+
+    public static String formatStringNumberWithCommaAndCurrency(String pStringValue) {
+        int lIntValue = Integer.parseInt(pStringValue);
+        return NumberFormat.getCurrencyInstance(Locale.getDefault()).format(lIntValue);
+    }
+    public static int formatStringNumberWithCommaAndCurrencyToInt(String pValue) {
+        pValue = pValue.replaceAll(",","");
+        pValue = pValue.substring(1, pValue.length()-3);
+
+        return  Integer.parseInt(pValue);
     }
 }
--- app/src/main/java/com/openclassrooms/realestatemanager/view/fragments/LoanFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/view/fragments/LoanFragment.java
@@ -2,7 +2,6 @@ package com.openclassrooms.realestatemanager.view.fragments;
 
 import android.content.Context;
 
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -10,21 +9,44 @@ import android.widget.Toast;
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.databinding.FragmentLoanBinding;
 import com.openclassrooms.realestatemanager.model.MonthlyPayment;
+import com.openclassrooms.realestatemanager.utils.REMHelper;
 
 
 public class LoanFragment extends BaseFragment<FragmentLoanBinding> {
 
     private FragmentLoanBinding mBinding;
+    private Context mContext;
 
     @Override
-    protected int getMenuAttached() { return R.menu.menu_confirm; }
+    protected int getMenuAttached() {
+        return R.menu.menu_confirm;
+    }
 
     @Override
-    protected int getFragmentLayout() { return R.layout.fragment_loan; }
+    protected int getFragmentLayout() {
+        return R.layout.fragment_loan;
+    }
 
     @Override
     protected void configureDesign(FragmentLoanBinding pBinding) {
         mBinding = pBinding;
+        View lFragView = mBinding.getRoot();
+        mContext = lFragView.getContext();
+
+        mBinding.fragLoanEtPrice.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                if (!mBinding.fragLoanEtPrice.getText().toString().equals("")) {
+                    mBinding.fragLoanEtPrice.setText(REMHelper.formatStringNumberWithCommaAndCurrency(mBinding.fragLoanEtPrice.getText().toString()));
+                }
+            }
+        });
+        mBinding.fragLoanEtContribution.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                if (!mBinding.fragLoanEtContribution.getText().toString().equals("")) {
+                    mBinding.fragLoanEtContribution.setText(REMHelper.formatStringNumberWithCommaAndCurrency(mBinding.fragLoanEtContribution.getText().toString()));
+                }
+            }
+        });
     }
 
     @Override
@@ -37,12 +59,43 @@ public class LoanFragment extends BaseFragment<FragmentLoanBinding> {
     }
 
     private void calculateMonthlyPayment() {
-        MonthlyPayment lPayment = new MonthlyPayment();
-        lPayment.setMpAmountBorrowed(Integer.parseInt(mBinding.fragLoanEtPrice.getText().toString()));
-        lPayment.setMpContribution(Integer.parseInt(mBinding.fragLoanEtContribution.getText().toString()));
-        lPayment.setMpRate(Double.parseDouble(mBinding.fragLoanEtRate.getText().toString()));
-        lPayment.setMpDuration(Integer.parseInt(mBinding.fragLoanEtDuration.getText().toString()));
 
-        mBinding.fragLoanTvMonthlyPaymentResult.setText(String.valueOf(lPayment.calculateMonthlyPayment()));
+        if (validInput()) {
+            MonthlyPayment lPayment = new MonthlyPayment();
+
+            int lPrice = REMHelper.formatStringNumberWithCommaAndCurrencyToInt(mBinding.fragLoanEtPrice.getText().toString());
+            lPayment.setMpAmountBorrowed(lPrice);
+
+            lPayment.setMpRate(Double.parseDouble(mBinding.fragLoanEtRate.getText().toString()));
+            lPayment.setMpDuration(Integer.parseInt(mBinding.fragLoanEtDuration.getText().toString()));
+
+            int lContribution =0;
+            if (!mBinding.fragLoanEtContribution.getText().toString().equals("")) {
+                lContribution = REMHelper.formatStringNumberWithCommaAndCurrencyToInt(mBinding.fragLoanEtContribution.getText().toString());
+            }
+            lPayment.setMpContribution(lContribution);
+            String lPay = REMHelper.formatNumberWithCommaAndCurrency(lPayment.calculateMonthlyPayment());
+
+            mBinding.fragLoanTvMonthlyPaymentResult.setText(lPay);
+        }
+    }
+
+    private boolean validInput() {
+        String lField = "";
+        if ((mBinding.fragLoanEtPrice.getText() == null) || (mBinding.fragLoanEtPrice.getText().toString().equals(""))) {
+            lField = REMHelper.addValueAndSeparatorToString(lField, ",", getString(R.string.txt_price));
+        }
+        if ((mBinding.fragLoanEtRate.getText() == null) || (mBinding.fragLoanEtRate.getText().toString().equals(""))) {
+            lField = REMHelper.addValueAndSeparatorToString(lField, ",", getString(R.string.loan_txt_err_field_rate));
+        }
+        if ((mBinding.fragLoanEtDuration.getText() == null) || (mBinding.fragLoanEtDuration.getText().toString().equals(""))) {
+            lField = REMHelper.addValueAndSeparatorToString(lField, ",", getString(R.string.loan_txt_err_field_duration));
+        }
+        if (lField.length() > 0) {
+            Toast.makeText(mContext, getString(R.string.loan_txt_err_valid_input) + " " + lField, Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
     }
 }
