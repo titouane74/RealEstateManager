d6e40460164892e05d682f5fc81598c2dc93de90
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-12-19 05:47:25 +0100

    Test Instrumented : internet connectivity : add the enabled/disabled internet for the API <= 28, deprecated for API 29

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/UtilsTest.java
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/UtilsTest.java
@@ -1,10 +1,15 @@
 package com.openclassrooms.realestatemanager;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.openclassrooms.realestatemanager.utils.Utils;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,8 +21,31 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class UtilsTest {
 
+    @Before
+    public void setup() throws InterruptedException {
+        if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            WifiManager wifiManager = (WifiManager) InstrumentationRegistry.getInstrumentation().getTargetContext().getSystemService(Context.WIFI_SERVICE);
+            wifiManager.setWifiEnabled(true);
+            wifiManager.disconnect();
+            Thread.sleep(1000 * 8);
+        }
+    }
+
     @Test
     public void isInternetAvailable() {
         assertEquals(true, Utils.isInternetAvailable(InstrumentationRegistry.getInstrumentation().getTargetContext()));
+    }
+
+    @Test
+    public void isWifiDisabled() throws InterruptedException {
+        if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            WifiManager wifiManager = (WifiManager) InstrumentationRegistry.getInstrumentation().getTargetContext().getSystemService(Context.WIFI_SERVICE);
+            wifiManager.setWifiEnabled(false);
+            wifiManager.disconnect();
+
+            Thread.sleep(1000 * 8);
+
+            assertEquals(false, Utils.isInternetAvailable(InstrumentationRegistry.getInstrumentation().getTargetContext()));
+        }
     }
 }
